214ac8c26bd63d8c24e346b585f2b5de6c0cebc8
Yuri Artiukh <akella.a@gmail.com>
2019-07-28 00:30:17 +0300

    emoji fontain

---
+++ src/js/Particle.js
@@ -0,0 +1,101 @@
+const rand = function (max, min, _int) {
+    const defmax = (max === 0 || max) ? max : 1;
+    const defmin = min || 0;
+    const gen = defmin + (defmax - defmin) * Math.random();
+
+    return (_int) ? Math.round(gen) : gen;
+};
+const PI = 3.1415;
+
+export default class Particle {
+    constructor(img, w, h, number) {
+        this.w = w;
+        this.h = h;
+        this.img = img;
+        this.active = false;
+        this.number = number;
+        this.size = 20;
+
+        this.delay = rand(this.number, 0, 1);
+        console.log(this.delay);
+        // this.delay = delay;
+    }
+
+    shoot(source, t) {
+        this.source = source;
+        this.t = t;
+        /* check if time for shooting this particle has arrived */
+        if (this.t - this.delay >= 0) {
+            /* make it active */
+            this.active = true;
+
+            /* choose our confetti */
+            // confetti = confettis[floor(random() * confettis.length)];
+
+            /* position it at the fountain source, 
+             * but a bit lower, depending on its radius
+             */
+            this.pos = {
+                x: this.source.x + rand(-10, 10),
+                y: this.source.y
+            };
+
+            /*
+             * give it an acceleration considering gravity
+             * and uniform friction (depending on its radius)
+             */
+            this.a = {
+                x: 0,
+                y: .4
+            };
+
+            /* generate a random angle at which it shoots up */
+            this.angle = rand(PI / 8, -PI / 8) - PI / 2;
+
+            /* Set up our confetti particle angle */
+            this.c_angle = 0;
+            this.angle_v = rand(-30, 30);
+
+            /* generate random velocity absolute value in that direction */
+            const h = 300;
+            const val = rand(h / 21, h / 60);
+
+            /* compute initial velocity components */
+            this.v = {
+                x: val * Math.cos(this.angle),
+                y: val * Math.sin(this.angle)
+            };
+        }
+    }
+
+    update() {
+        this.v.x += this.a.x;
+        this.v.y += this.a.y;
+        this.pos.x += Math.round(this.v.x);
+        this.pos.y += Math.round(this.v.y);
+        this.c_angle += this.angle_v;
+        const w = this.width;
+        const h = this.height;
+
+        /* if it has landed = it's below canvas bottom edge */
+        if (this.pos.y > this.h || this.pos.x < 0 || this.pos.x > this.w) {
+            /* reset position to the initial one */
+            this.pos = {
+                x: this.source.x,
+                y: this.source.y
+            };
+            /* ... and make this particle inactive */
+            this.active = false;
+        }
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.pos.x, this.pos.y);
+        ctx.rotate(this.c_angle * Math.PI / 180);
+        ctx.drawImage(this.img, -this.size / 2, -this.size / 2, this.size, this.size);
+        ctx.restore();
+
+        this.update();
+    }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,103 @@
-import sayHello from './lib/sayHello';
+import Particle from './Particle';
 
-sayHello();
+function loadImages(paths, whenLoaded) {
+    const imgs = [];
+    paths.forEach(function (path) {
+        const img = new Image();
+        img.onload = function () {
+            imgs.push(img);
+            if (imgs.length === paths.length) whenLoaded(imgs);
+        };
+        img.src = path;
+    });
+}
+
+export default class Sketch {
+    constructor() {
+        this.selector = '.fontain'
+        this.time = 0;
+        this.parent = document.querySelector(this.selector);
+
+        this.canvas = document.createElement('canvas');
+        this.parent.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+
+        this.particles = [];
+        this.source = {
+            x: this.width / 2,
+            y: this.height
+        };
+        this.number = 100;
+
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
+        loadImages(['img/emoji.png'], (images) => {
+            this.img = images;
+
+            this.resize();
+            this.mouse();
+            this.addParticles();
+            this.animate();
+
+        })
+
+
+    }
+
+    mouse() {
+        const that = this;
+        document.addEventListener('mousemove', function (e) {
+
+            /* move x coordinate of particle fountain source */
+            that.source.x = e.clientX;
+            that.source.y = e.clientY;
+        }, false);
+    }
+
+
+    resize() {
+        window.addEventListener('resize', () => {
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+            this.canvas.style.width = this.width;
+            this.canvas.style.height = this.height;
+            this.particles.forEach(p=>{
+            	const newp = p;
+            	newp.width = this.width;
+            	newp.height = this.height;
+            })
+        })
+    }
+
+    addParticles() {
+        for (let i = 0; i < this.number; i++) {
+            this.particles.push(
+                new Particle(this.img[0], this.width, this.height, this.number)
+            );
+        }
+    }
+
+    animate() {
+        this.time += 1;
+        this.ctx.clearRect(0, 0, this.width, this.height);
+
+
+
+        for (let i = 0; i < this.number; i++) {
+            if (this.particles[i].active) { // if it's active
+                this.particles[i].draw(this.ctx); // draw it on canvas
+            } else { // if not...
+                this.particles[i].shoot(this.source, this.time); // try to make it shoot up
+            }
+        }
+        const newar = this.particles.filter(e=>e.active);
+
+
+        window.requestAnimationFrame(this.animate.bind(this))
+    }
+}
+
+const b = new Sketch();
