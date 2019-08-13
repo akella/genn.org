274986d973d947883a19b5755edc8a8e51c77437
genn <github@genn.org>
2019-08-13 22:27:29 +0300

    All the emojis are there

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,4 +1,4 @@
-const helloHeight = document.querySelector('.hello').clientHeight;
+const hello = document.querySelector('.hello');
 const header = document.querySelector('.header');
 const headerWelcome = document.querySelector('.header__text__welcome');
 const logo = document.querySelector('.logo');
@@ -11,14 +11,21 @@ const contactRect = document.querySelector('.contact').getBoundingClientRect();
 
 window.addEventListener("scroll", function (event) {
 	const scroll = this.scrollY;
-    const helloHidden = this.scrollY-window.innerHeight/2;
-    if(helloHidden > 0) {
+    // const helloHidden = this.scrollY-window.innerHeight/2;
+/*    if(helloHidden > 0) {
     	header.classList.remove('header__pinned');
     } else {
     	header.classList.add('header__pinned');
-    }
+    } */
 
-    if(scroll>skillsRect.top-window.innerHeight/2-48 && scroll<skillsRect.bottom-window.innerHeight/2-48) {
+    const helloOpacity = 1-scroll/400;
+
+    if(helloOpacity>0) {hello.style.opacity = helloOpacity;}
+    else {hello.style.opacity = 0;}
+
+
+
+    if(scroll>skillsRect.top-window.innerHeight/2-48 && scroll<skillsRect.bottom-window.innerHeight/2+48) {
     	logo.style.fill='#ffffff';
     } else logo.style.fill='#ff5500';
 
